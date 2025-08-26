cf034b2b39a9280357ee5c5db849ca27807d37aa
SHOBHITGITS <shobhits103@gmail.com>
2025-08-26 18:51:44 +0530

    Remove dark mode button

--- src/App.js
+++ src/App.js
@@ -21,17 +21,8 @@ function App() {
         }
     }, [darkMode]);
   return (
-      <div className="min-h-screen bg-gray-900 text-white dark:bg-white dark:text-black transition-colors duration-300">
+    <div>
       <NavBar/>
-        {/* Toggle Button */}
-        <div className="fixed top-4 right-4 z-50">
-            <button
-                onClick={() => setDarkMode(!darkMode)}
-                className="px-4 py-2 rounded-lg bg-gray-200 dark:bg-gray-700 dark:text-white shadow-md"
-            >
-                {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
-            </button>
-        </div>
       <Home/>
       <About/>
       <CareerJourney/>
