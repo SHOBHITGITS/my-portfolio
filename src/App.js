b42d23ee0e387267b27350ad2ad00b6e19c77a2f
SHOBHITGITS <shobhits103@gmail.com>
2025-08-26 18:52:20 +0530

    Remove dark mode

--- src/App.js
+++ src/App.js
@@ -9,17 +9,7 @@ import Skills from "./components/Skills";
 import Footer from './components/Footer';
 import CareerJourney from './components/CareerJourney';
 import ScrollToTop from './components/ScrollToTop';
-import React, { useState, useEffect } from "react";
 function App() {
-    const [darkMode, setDarkMode] = useState(true); // 👈 default dark
-
-    useEffect(() => {
-        if (darkMode) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    }, [darkMode]);
   return (
     <div>
       <NavBar/>
